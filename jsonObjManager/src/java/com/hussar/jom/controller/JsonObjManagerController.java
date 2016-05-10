e64ea556fdc5bbbf78d9e375d19b48244265f16d
hussarch <hussarch@163.com>
2016-05-10 18:15:12 +0800

    Firest step

---
+++ jsonObjManager/src/java/com/hussar/jom/controller/JsonObjManagerController.java
@@ -0,0 +1,19 @@
+package com.hussar.jom.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author yi.xiao
+ * Created on 2016年5月10日 下午5:12:36
+ * © 2015 Fanli.com
+ * 
+ */
+@Controller("/obj/json")
+public class JsonObjManagerController {
+    
+    
+    
+    
+    
+    
+}
