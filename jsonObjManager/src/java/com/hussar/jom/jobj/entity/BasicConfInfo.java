184495ab624e5237620b95979c2a9a04f13a2d15
hussarch <hussarch@163.com>
2016-05-23 22:22:53 +0800

    add the index

---
+++ jsonObjManager/src/java/com/hussar/jom/jobj/entity/BasicConfInfo.java
@@ -0,0 +1,14 @@
+package com.hussar.jom.jobj.entity;
+
+/**
+ * @CommonConfInfo.java
+ * @author XiaoYi(hussarch@126.com)
+ * Created on 2016-5-21, ©2016 some rights reserved
+ */
+public class BasicConfInfo {
+	
+	private String startTime;
+	private String endTime;
+	
+	
+}
